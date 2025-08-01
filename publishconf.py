9c75e6a158a7b18973cdea5e32fef7dc1f9c631c
CatKhakis <violet@funkycat.media>
2025-07-31 23:18:59 -0500

    aaaaa

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://catkhakis.github.io/zoeySite/"
+SITEURL = "https://catkhakis.github.io/zoeySite"
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
